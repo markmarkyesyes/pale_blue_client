5441c703e5da668a4e6ea6f3c74dee42db39550d
Jerry Gao <gao@blackwright.com>
2017-06-13 18:54:17 -0400

    pass userLocation prop through AppContainer and App

--- src/components/App.js
+++ src/components/App.js
@@ -2,23 +2,26 @@ import React from "react";
 import CesiumGlobe from "./CesiumGlobe";
 import Authentication from "./UI/Authentication";
 
+const containerStyle = {
+  width: '100%',
+  height: '100%',
+  top: 0,
+  left: 0,
+  bottom: 0,
+  right: 0,
+  position: 'fixed'
+};
+
 export default class App extends React.Component {
   render() {
+    const { userLocation } = this.props;
 
-    const containerStyle = {
-      width: '100%',
-      height: '100%',
-      top: 0,
-      left: 0,
-      bottom: 0,
-      right: 0,
-      position: 'fixed'
-    };
+    if (!userLocation) return null;
 
     return (
       <div style={containerStyle}>
         <div id="credits" />
-        <CesiumGlobe />
+        <CesiumGlobe userLocation={userLocation} />
         <div style={{position: 'fixed', top: 10, left: 10}}>
           <Authentication />
         </div>
