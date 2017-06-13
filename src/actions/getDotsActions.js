829e7934b57b20d58b9ccff6cbc611041768bbf5
Jerry Gao <gao@blackwright.com>
2017-06-13 18:56:07 -0400

    rename actions to be more descriptive in getDotsActions

--- src/actions/getDotsActions.js
+++ src/actions/getDotsActions.js
@@ -1,24 +1,24 @@
-export const REQUEST_START = 'REQUEST_START';
-export const REQUEST_SUCCESS = 'REQUEST_SUCCESS';
-export const REQUEST_FAILURE = 'REQUEST_FAILURE';
+export const GET_DOTS_START = 'GET_DOTS_START';
+export const GET_DOTS_SUCCESS = 'GET_DOTS_SUCCESS';
+export const GET_DOTS_FAILURE = 'GET_DOTS_FAILURE';
 
 
 export function requestStart() {
   return {
-    type: REQUEST_START
+    type: GET_DOTS_START
   };
 }
 
 export function requestSuccess(data) {
   return {
-    type: REQUEST_SUCCESS,
+    type: GET_DOTS_SUCCESS,
     data
   };
 }
 
 export function requestFailure(error) {
   return {
-    type: REQUEST_FAILURE,
+    type: GET_DOTS_FAILURE,
     error
   };
 }
