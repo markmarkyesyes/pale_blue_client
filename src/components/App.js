69208c65433f6941b10641f318603caf483fecb4
Jerry Gao <gao@blackwright.com>
2017-06-12 22:50:59 -0400

    reposition DOM elements

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,24 @@ import Authentication from "./UI/Authentication";
 
 export default class App extends React.Component {
   render() {
+
+    const containerStyle = {
+      width: '100%',
+      height: '100%',
+      top: 0,
+      left: 0,
+      bottom: 0,
+      right: 0,
+      position: 'fixed'
+    };
+
     return (
-      <div>
+      <div style={containerStyle}>
         <div id="credits" />
         <CesiumGlobe />
-        <Authentication />
+        <div style={{position: 'fixed', top: 10, left: 10}}>
+          <Authentication />
+        </div>
       </div>
     );
   }
