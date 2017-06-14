e84c8a88d0a20c52ae13cc570d83983303321997
markmarkyesyes <markhahn545@gmail.com>
2017-06-13 22:03:50 -0500

    removed unused keys from session state

--- src/reducers/session.js
+++ src/reducers/session.js
@@ -1,8 +1,6 @@
 import * as Actions from "../actions/sessionActions";
 
 const initialState = {
-  isFetching: false,
-  isAuthed: false,
   data: {},
   error: null
 };
@@ -12,45 +10,33 @@ export function session(state = initialState, action) {
     case Actions.LOGIN_START:
       return {
         ...state,
-        isFetching: true,
-        isAuthed: false,
         error: null
       };
     case Actions.LOGIN_SUCCESS:
       return {
         ...state,
-        isFetching: false,
-        isAuthed: true,
         error: null,
         data: { _id: action.data }
       };
     case Actions.LOGIN_FAILURE:
       return {
         ...state,
-        isFetching: false,
-        isAuthed: false,
         error: action.error
       };
     case Actions.REG_START:
       return {
         ...state,
-        isFetching: true,
-        isAuthed: false,
         error: null
       };
     case Actions.REG_SUCCESS:
       return {
         ...state,
-        isFetching: false,
-        isAuthed: true,
         error: null,
         data: { _id: action.data }
       };
     case Actions.REG_FAILURE:
       return {
         ...state,
-        isFetching: false,
-        isAuthed: false,
         error: action.error
       };
     default:
