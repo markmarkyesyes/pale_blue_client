6da95040905df9defc9d1e71693d299ad1edaae6
Jerry Gao <gao@blackwright.com>
2017-06-14 00:26:28 -0400

    add camera reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,8 +3,10 @@ import { submit } from "./submit";
 import { session } from './session';
 import { dotsList } from './dotsList';
 import { userLocation } from './userLocation';
+import { camera } from './camera';
 
 export default combineReducers({
+  camera,
   userLocation,
   session,
   dotsList,
