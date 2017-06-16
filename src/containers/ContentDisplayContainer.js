266a53b67e9e55fd9a4843199dd8a406adf6c674
Jerry Gao <gao@blackwright.com>
2017-06-15 22:08:39 -0400

    comment out circles for posterity

--- src/containers/ContentDisplayContainer.js
+++ src/containers/ContentDisplayContainer.js
@@ -1,7 +1,7 @@
 import { connect } from 'react-redux';
 import ContentDisplay from '../components/ContentDisplay';
 import Cartesian3 from 'cesium/Source/Core/Cartesian3';
-import Color from 'cesium/Source/Core/Color';
+// import Color from 'cesium/Source/Core/Color';
 
 function distanceBetween(location1, location2) {
   return Cartesian3.distance(location1, location2);
@@ -11,37 +11,35 @@ function nearbyContent(cameraLocation, dotsList, radius) {
   if (!cameraLocation || !dotsList) {
     return [];
   }
-
-  const content = dotsList.filter(dot => {
+  return dotsList.filter(dot => {
     const dotLocation = Cartesian3.fromDegrees(dot.lng, dot.lat);
     return distanceBetween(cameraLocation, dotLocation) < radius;
   });
-
-  console.log(content);
-  return content;
 }
 
 const mapStateToProps = (state, ownProps) => {
   const cameraLocation = state.camera.location;
   const dotsList = state.dotsList.data;
-  const { scene, viewer } = ownProps;
+  const { scene } = ownProps;
 
   const height = scene.camera.positionCartographic.height;
   const radius = height / 10;
 
-  viewer.entities.add({
-    ellipse: {
-      semiMajorAxis: radius,
-      semiMinorAxis: radius,
-      material: Color.WHITE.withAlpha(0.2),
-      outline: Color.WHITE,
-      height: 0
-    },
-    position: cameraLocation
-  });
+  // the legacy of the white circles
+
+  // viewer.entities.add({
+  //   ellipse: {
+  //     semiMajorAxis: radius,
+  //     semiMinorAxis: radius,
+  //     material: Color.WHITE.withAlpha(0.2),
+  //     outline: Color.WHITE,
+  //     height: 0
+  //   },
+  //   position: cameraLocation
+  // });
 
   return {
-    content: nearbyContent(cameraLocation, dotsList, radius)
+    nearbyContent: nearbyContent(cameraLocation, dotsList, radius)
   };
 }
 
