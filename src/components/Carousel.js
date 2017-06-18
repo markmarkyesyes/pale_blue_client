bc599a612a1fd319c38bd4855c3b4127692b9fe0
Jerry Gao <gao@blackwright.com>
2017-06-18 19:27:29 -0400

    Carousel component displays slick slider properly

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -2,13 +2,14 @@ import React from "react";
 import Slider from "react-slick";
 import Text from "./content/Text";
 import Image from "./content/Image";
+import SlideWrapper from './content/SlideWrapper';
+import FullContentContainer from '../containers/FullContentContainer';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const config = {
   arrows: false,
   centerMode: true,
-  centerPadding: "15px",
   dots: false,
   focusOnSelect: true,
   infinite: false,
@@ -19,7 +20,6 @@ const config = {
       settings: {
         slidesToShow: 1,
         centerMode: true,
-        centerPadding: "15px"
       }
     },
     {
@@ -27,7 +27,6 @@ const config = {
       settings: {
         slidesToShow: 3,
         centerMode: true,
-        centerPadding: "15px"
       }
     },
     {
@@ -35,7 +34,6 @@ const config = {
       settings: {
         slidesToShow: 5,
         centerMode: true,
-        centerPadding: "15px"
       }
     }
   ],
@@ -44,51 +42,86 @@ const config = {
   useCSS: true
 };
 
-function numberOfSlides(nearbyContent) {
-  if (nearbyContent.length <= 3) return nearbyContent.length;
-  return 4;
-}
-
 const slideStyle = {
   height: 120
 };
 
-export default function Carousel(contentFound, nearbyContent) {
-  if (!contentFound) return null;
+export default class Carousel extends React.Component {
+  state = { selectedContent: null };
 
-  const slides = nearbyContent.map(dot => {
-    switch (dot.contentType) {
-      case "text":
-        return (
-          <div style={slideStyle} key={dot._id}>
-            <Text data={dot.data} />
-          </div>
-        );
-      case "image":
-        return (
-          <div style={slideStyle} key={dot._id}>
-            <Image data={dot.data} />
-          </div>
-        );
-      default:
-        return null;
-    }
-  });
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleEscape, false);
+  }
 
-  return (
-    <div
-      style={{
-        position: "fixed",
-        top: 75,
-        left: 0,
-        zIndex: 99999,
-        width: "100%",
-        backgroundColor: "rgba(255, 255, 255, 0.5)"
-      }}
-    >
-      <Slider {...config}>
-        {slides}
-      </Slider>
-    </div>
-  );
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleEscape);
+  }
+
+  componentWillReceiveProps(newProps) {
+    this.closeContent();
+  }
+
+  selectContent = content => {
+    this.setState({ selectedContent: content });
+  }
+
+  closeContent = () => {
+    this.setState({ selectedContent: null });
+  }
+
+  handleEscape = e => {
+    if (e.keyCode === 27) this.closeContent();
+  }
+
+  render() {
+    const { contentFound, nearbyContent } = this.props;
+    const { selectedContent } = this.state;
+
+    if (!contentFound) return null;
+
+    const slides = nearbyContent.map(dot => {
+      switch (dot.contentType) {
+        case "text":
+          return (
+            <div style={slideStyle} key={dot._id}>
+              <SlideWrapper selectContent={this.selectContent} dot={dot}>
+                <Text data={dot.data} />
+              </SlideWrapper>
+            </div>
+          );
+        case "image":
+          return (
+            <div style={slideStyle} key={dot._id}>
+              <SlideWrapper selectContent={this.selectContent} dot={dot}>
+                <Image data={dot.data} />
+              </SlideWrapper>
+            </div>
+          );
+        default:
+          return null;
+      }
+    });
+
+    return (
+      <div
+        style={{
+          position: "fixed",
+          top: 75,
+          left: 0,
+          zIndex: 99999,
+          width: "100%",
+          backgroundColor: "rgba(255, 255, 255, 0.5)"
+        }}
+      >
+        <Slider {...config}>
+          {slides}
+        </Slider>
+        <FullContentContainer
+          selectedContent={selectedContent}
+          closeContent={this.closeContent}
+        />
+      </div>
+    );
+
+  }
 }
