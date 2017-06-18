13478e7052c1f7a87e04f7e21ad7a74cfd39156f
Jerry Gao <gao@blackwright.com>
2017-06-18 19:34:32 -0400

    FullContent displays text and image content with close and like buttons

---
+++ src/components/content/FullContent.js
@@ -0,0 +1,107 @@
+import React from "react";
+
+const fullPanelStyle = {
+  position: "fixed",
+  top: 68,
+  left: 0,
+  width: "100%",
+  height: "calc(100% - 136px)",
+  padding: "2rem 1.5rem",
+  lineHeight: "1.5rem",
+  backgroundColor: "#e3e3e3",
+  overflowY: "scroll",
+  zIndex: 1000000
+};
+
+export default class FullContent extends React.Component {
+  handleLike = () => {
+    const {
+      selectedContent,
+      userLocation,
+      userId,
+      submitLike,
+      closeContent
+    } = this.props;
+    if (userLocation.lng && userLocation.lat && userId && selectedContent._id) {
+      const like = {
+        fromLng: userLocation.lng,
+        fromLat: userLocation.lat,
+        fromUserId: userId,
+        contentId: selectedContent._id
+      };
+      submitLike(like);
+    } else {
+      console.error("Missing like parameters");
+    }
+    closeContent();
+  };
+
+  render() {
+    const { selectedContent, closeContent } = this.props;
+
+    if (!selectedContent) return null;
+
+    return (
+      <div style={fullPanelStyle}>
+        <img
+          onClick={closeContent}
+          src="../../close.svg"
+          alt='close'
+          style={{
+            cursor: "pointer",
+            position: "absolute",
+            top: 8,
+            right: 20
+          }}
+        />
+        <img
+          onClick={this.handleLike}
+          src="../../like.svg"
+          alt='like'
+          style={{
+            cursor: "pointer",
+            position: "fixed",
+            height: 50,
+            width: 50,
+            bottom: 43,
+            left: "50%",
+            transform: "translateX(-50%)"
+          }}
+        />
+        {switchContent(selectedContent)}
+      </div>
+    );
+  }
+}
+
+function switchContent(dot) {
+  switch (dot.contentType) {
+    case "text":
+      return fullTextContent(dot.data);
+    case "image":
+      return fullImageContent(dot.data);
+    default:
+      return null;
+  }
+}
+
+function fullTextContent(data) {
+  return <p>{data}</p>;
+}
+
+function fullImageContent(data) {
+  return (
+    <img
+      src={data}
+      alt=''
+      style={{
+        position: "relative",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        maxWidth: "100%",
+        maxHeight: "100%"
+      }}
+    />
+  );
+}
