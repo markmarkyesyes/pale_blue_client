160288cdc462b1589267a50e6a1a68bc4e3ff8da
Jerry Gao <gao@blackwright.com>
2017-06-18 19:28:11 -0400

    Reticle has responsive behavior with window resize event listener

--- src/components/Reticle.js
+++ src/components/Reticle.js
@@ -1,32 +1,58 @@
 import React from 'react';
 
-const desktopRadius = window.innerHeight / 7.5;
-const mobileRadius = window.innerWidth / 7.5;
-const radius = window.innerHeight < window.innerWidth ? desktopRadius : mobileRadius;
-
 const borderWidth = 7;
 const DEEPSKYBLUE = '#00BFFF';
 const GOLD = "#FFD700";
 
-export default function Reticle(contentFound) {
-  const borderColor = contentFound ? GOLD : DEEPSKYBLUE;
+function getRadiusByViewportSize() {
+  const desktopRadius = window.innerHeight / 7.5;
+  const mobileRadius = window.innerWidth / 7.5;
+  if (window.innerHeight < window.innerWidth) {
+    return desktopRadius;
+  } else {
+    return mobileRadius;
+  }
+}
 
-  return (
-    <div style={{
-      pointerEvents: 'none',
-      position: 'fixed',
-      top: '50%',
-      left: '50%',
-      marginLeft: -radius,
-      marginTop: -radius,
-      width: radius * 2 - borderWidth * 2,
-      height: radius * 2 - borderWidth * 2,
-      borderRadius: '50%',
-      borderStyle: 'double',
-      borderWidth,
-      borderColor,
-      zIndex: '99999'
-    }}
-    />
-  );
+export default class Reticle extends React.Component {
+  state = { radius: getRadiusByViewportSize() };
+
+  componentDidMount() {
+    window.addEventListener("resize", this.recalculateRadius);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.recalculateRadius);
+  }
+
+  recalculateRadius = () => {
+    const radius = getRadiusByViewportSize();
+    this.setState({ radius });
+  };
+
+  render() {
+    const { contentFound } = this.props;
+    const { radius } = this.state;
+
+    const borderColor = contentFound ? GOLD : DEEPSKYBLUE;
+
+    return (
+      <div style={{
+        pointerEvents: 'none',
+        position: 'fixed',
+        top: '50%',
+        left: '50%',
+        marginLeft: -radius,
+        marginTop: -radius,
+        width: radius * 2 - borderWidth * 2,
+        height: radius * 2 - borderWidth * 2,
+        borderRadius: '50%',
+        borderStyle: 'double',
+        borderWidth,
+        borderColor,
+        zIndex: '99999'
+      }}
+      />
+    );
+  }
 }
