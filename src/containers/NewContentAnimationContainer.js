8274951700f462a44e58606231365c6fbe9809df
Jerry Gao <gao@blackwright.com>
2017-06-18 20:11:07 -0400

    refactor pulse into helper function, set multiply color

--- src/containers/NewContentAnimationContainer.js
+++ src/containers/NewContentAnimationContainer.js
@@ -1,74 +1,21 @@
 import React from "react";
 import socket from "../websockets";
 import Color from "cesium/Source/Core/Color";
-import Cartesian3 from "cesium/Source/Core/Cartesian3";
-import defined from "cesium/Source/Core/defined";
-import CallbackProperty from "cesium/Source/DataSources/CallbackProperty";
+import { pulse } from '../helpers/animation';
+
+const multiplyColor = Color.GOLD;
 
 class NewContentAnimationContainer extends React.Component {
-  ////
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+
+    const { viewer } = props;
+
     socket.on("new content", content => {
-      console.log("new content");
-      this.pulse(content.lng, content.lat);
-      setTimeout(() => {
-        this.pulse(content.lng, content.lat);
-      }, 1000);
-      setTimeout(() => {
-        this.pulse(content.lng, content.lat);
-      }, 2000);
-      setTimeout(() => {
-        this.pulse(content.lng, content.lat);
-      }, 3000);
-      setTimeout(() => {
-        this.pulse(content.lng, content.lat);
-      }, 4000);
+      pulse(viewer, content.lng, content.lat, multiplyColor);
     });
   }
-  ////
-  expand = rate => {
-    const start = performance.now();
-    return new CallbackProperty((time, scale) => {
-      if (!defined(scale)) {
-        scale = 1.0;
-      }
-      const now = performance.now();
-      const t = now - start;
-      return scale + t / rate;
-    }, false);
-  };
-  ////
-  fadeOut = duration => {
-    const start = performance.now();
-    return new CallbackProperty((time, color) => {
-      const now = performance.now();
-      const t = now - start;
-      let alpha = 1.0 - t / duration;
-      if (alpha < 0) alpha = 0;
-      return new Color(1.0, 1.0, 1.0, alpha);
-    }, false);
-  };
-  ////
-  pulse = (lng, lat) => {
-    const duration = 4000;
-    const rate = 250;
 
-    const circle = this.props.viewer.entities.add({
-      position: Cartesian3.fromDegrees(lng, lat),
-      billboard: {
-        image: require("../../public/circle.svg"),
-        width: 20,
-        height: 20,
-        scale: this.expand(rate),
-        color: this.fadeOut(duration)
-      }
-    });
-    setTimeout(() => {
-      this.props.viewer.entities.remove(circle);
-    }, duration);
-  };
-  ////
   render() {
     return null;
   }
