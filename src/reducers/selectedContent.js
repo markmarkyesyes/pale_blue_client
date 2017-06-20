5cd4f8b58715a4bbd0c90c0b42028d5af0694cce
Jerry Gao <gao@blackwright.com>
2017-06-20 16:01:16 -0400

    add selectedContent reducer

---
+++ src/reducers/selectedContent.js
@@ -0,0 +1,14 @@
+import * as Actions from '../actions/selectedContent';
+
+const initialState = null;
+
+export function selectedContent(state = initialState, action) {
+  switch (action.type) {
+    case Actions.SELECT_CONTENT:
+      return action.data;
+    case Actions.CLOSE_CONTENT:
+      return null;
+    default:
+      return initialState;
+  }
+}
