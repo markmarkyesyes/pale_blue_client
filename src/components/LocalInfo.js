729f7b50272e047aeb4a0bf03a38234751213ab5
Jerry Gao <gao@blackwright.com>
2017-06-21 20:44:33 -0400

    add checks to prevent unnecessary API calls

--- src/components/LocalInfo.js
+++ src/components/LocalInfo.js
@@ -6,17 +6,41 @@ const pStyle = {
   color: "#00BFFF"
 };
 
+function selectedLocationChanged(nextProps, oldProps) {
+  if (!nextProps.selectedContent && !oldProps.selectedContent) {
+    return false;
+  }
+  return (nextProps.selectedContent && !oldProps.selectedContent) ||
+         (!nextProps.selectedContent && oldProps.selectedContent) ||
+         (nextProps.selectedContent.lng !== oldProps.selectedContent.lng) ||
+         (nextProps.selectedContent.lat !== oldProps.selectedContent.lat);
+}
+
+function nearbyLocationChanged(nextProps, oldProps) {
+  if (!nextProps.nearbyContent && !oldProps.nearbyContent) {
+    return false;
+  }
+  return (nextProps.nearbyContent[0] && !oldProps.nearbyContent[0]) ||
+         (!nextProps.nearbyContent[0] && oldProps.nearbyContent[0]) ||
+         (nextProps.nearbyContent[0].lng !== oldProps.nearbyContent[0].lng) ||
+         (nextProps.nearbyContent[0].lat !== oldProps.nearbyContent[0].lat);
+}
+
 export default class LocalInfo extends Component {
   state = { results: null };
 
-  componentWillReceiveProps(nextProps, oldProps) {
+  componentWillReceiveProps(nextProps) {
     if (!nextProps.nearbyContent.length) {
       this.setState({ results: null });
       return;
     }
 
-    const { lng, lat } = nextProps.selectedContent || nextProps.nearbyContent[0];
+    if (!selectedLocationChanged(nextProps, this.props) &&
+        !nearbyLocationChanged(nextProps, this.props)) {
+      return;
+    }
 
+    const { lng, lat } = nextProps.selectedContent || nextProps.nearbyContent[0];
     getLocalInfo(lng, lat).then(results => this.setState({ results }));
   }
 
